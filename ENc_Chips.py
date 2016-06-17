272d375e9df6be693bffd0c15c2e982c352dd961
maurerax <maurerax@gmail.com>
2016-06-17 14:15:39 -0400

    Update and rename runchips.py to ENc_Chips.py
    Brief update with usage info and ability to run it from command line or queried for naïve users

---
+++ ENc_Chips.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+print 'This calculates the codon bias of your input file - change file name in script. '
+print 'for information about chips, see http://emboss.toulouse.inra.fr/cgi-bin/emboss/help/chips'
+
+
+import os
+from Bio import SeqIO
+import sys
+
+if len(sys.argv) > 1:
+	infile = sys.argv[1]
+else:
+	infile = raw_input('What is your FASTA file named?  ')
+inseq = SeqIO.parse(infile,'fasta')
+for seq in inseq:
+    outfile = open('test.fas','w')
+    outfile.write('>' + re.sub('\|','_',seq.id) + '\n' + str(seq.seq) + '\n')
+    outfile.close()
+    os.system('chips -seqall test.fas -outfile ' + seq.id + '.chips')
+os.system('rm test.fas')
+outfile = open(infile + '_chips.txt','w')
+for f in  os.listdir(os.curdir):
+	if f.split('.')[-1] == 'chips':
+		infile = open(f,'r')
+		for line in infile:
+			if re.search('Nc',line):
+				NC = line.split()[-1] 
+		outfile.write(f + ',' + NC + '\n')
+outfile.close()
+for filename in os.listdir(os.curdir):
+	if filename.endswith('.chips'):
+		os.system('rm '+filename)	
